2de380b950526be7eaf986b1bac6889dcdcdba97
Giulio <giulio.sanna@outlook.it>
2023-05-18 16:31:07 +0200

    added till 5

--- GreetingTest.cs
+++ GreetingTest.cs
@@ -1,10 +1,12 @@
 
+using Xunit.Abstractions;
+
 namespace Greeting.Test
 {
     public class GreetingTest
     {
         private readonly Greeting _sut;
-
+        private readonly ITestOutputHelper _output;
         public GreetingTest()
         {//ctor del _sut
             _sut = new Greeting();
@@ -24,17 +26,31 @@ namespace Greeting.Test
             Assert.Equal("Hello, my friend.", result);
         }
 
-        //[Fact]
-        //public void IsUpperCaseTest() 
-        //{
-        //    var result = _sut.Greet("JERRY");
-        //    Assert.Equal("HELLO JERRY", result);
-        //}
+        [Fact]
+        public void IsUpperCaseTest()
+        {
+            var result = _sut.Greet("JERRY");
+            Assert.Equal("HELLO, JERRY!", result);
+        }
+
+        [Fact]
+        public void TwoNames()
+        {
+            var result = _sut.Greet("Jill","Jane");
+            Assert.Equal("Hello, Jill and Jane.", result);
+        }
 
         //[Fact]
-        //public void MoreName()
+        //public void SandBox()
         //{
-        //    var result = _sut.Greet({"Jill","Jane"});
+        //    var result = _sut.Greet("Andre", "Paperino");
+        //    _output.WriteLine(result);
         //}
+        [Fact]
+        public void ALotOfNames()
+        {
+            var result = _sut.Greet("Amy", "Brian", "Charlotte");
+            Assert.Equal("Hello, Amy, Brian, and Charlotte.", result);
+        }
     }
 }
